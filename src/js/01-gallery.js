b56fdd5888a4e001ff3cd21dff5e5d4afbc45397
Andreas-206 <puma_real@ukr.net>
2023-12-18 18:50:41 +0200

    work

---
+++ src/js/01-gallery.js
@@ -0,0 +1,19 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    { small: "small-image1.jpg", large: "large-image1.jpg", alt: "Image 1" },
+    { small: "small-image2.jpg", large: "large-image2.jpg", alt: "Image 2" },
+  ];
+
+  const gallery = document.querySelector(".gallery");
+  images.forEach(image => {
+    const galleryItem = document.createElement("li");
+    galleryItem.innerHTML = `<a class="galleri-link" href="${image.large}">
+      <img class="gallery-image" src="${image.small}" alt="${image.alt}"></a>`;
+    gallery.appendChild(galleryItem);
+  });
+
+  new SimpleLightbox("gallery a");
+});
