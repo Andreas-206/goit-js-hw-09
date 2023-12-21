e95690ecc2819ee5ff76b05506315f28576dbd5c
Andreas-206 <puma_real@ukr.net>
2023-12-21 20:25:45 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,34 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('.feedback-form');
-  const storageKey = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
 
-  const loadForm = () => JSON.parse(localStorage.getItem(storageKey)) || {};
-  const saveForm = () => {
-    const formData = {
-      email: form.email.value.trim(),
-      message: form.message.value.trim(),
-    };
-    localStorage.setItem(storageKey, JSON.stringify(formData));
+const loadForm = () => JSON.parse(localStorage.getItem(storageKey)) || {};
+const saveForm = () => {
+  const formData = {
+    email: form.email.value.trim(),
+    message: form.message.value.trim(),
   };
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+};
 
-  const populateForm = () => {
-    const { email = '', message = '' } = loadForm();
-    form.email.value = email;
-    form.message.value = message;
-  };
+const populateForm = () => {
+  const { email = '', message = '' } = loadForm();
+  form.email.value = email;
+  form.message.value = message;
+};
 
-  populateForm();
+populateForm();
 
-  form.addEventListener('input', saveForm);
+form.addEventListener('input', () => {
+  saveForm();
+});
 
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const { email, message } = loadForm();
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const { email, message } = loadForm();
 
-    if (email && message) {
-      console.log({ email, message });
-      localStorage.removeItem(storageKey);
-      form.reset();
-    };
-  });
+  if (email && message) {
+    console.log({ email, message });
+    localStorage.removeItem(storageKey);
+    form.reset();
+  }
 });
